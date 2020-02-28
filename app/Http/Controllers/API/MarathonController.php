9a14ddb528d6efde96b6f4fc818a76c923e466b7
Ernest <emuroiwa@gmail.com>
2020-02-28 10:03:41 +0200

    HAMENO

--- app/Http/Controllers/API/MarathonController.php
+++ app/Http/Controllers/API/MarathonController.php
@@ -40,19 +40,19 @@ class MarathonController extends Controller
      */
     public function show($id)
     {
-        $marathonTickets = Orders::leftJoin('customers', function($join){
-            $join->on('customers.event_id', '=', 'orders.category_id')
-                 ->where('customers.order_id', '=', 11111);
-        })       ->leftJoin('price_sub_categories', 'price_sub_categories.id', '=', 'orders.category_id')
-        ->leftJoin('zim_events', 'price_sub_categories.event_id', '=', 'zim_events.id')
-        ->leftJoin('event_types', 'zim_events.event_type_id', '=', 'event_types.id')
-        ->leftJoin('event_locations', 'event_locations.event_id', '=', 'zim_events.id')
-        ->select('orders.quantity','zim_events.event_name','price_sub_categories.description','orders.category_id AS catID','customers.user_id')
-        ->where('orders.user_id','=',$id)
-        ->where('orders.status','=',0)
-        ->where('event_types.event_type','=','marathon')
-        ->get();
-        return $marathonTickets;
+        // $marathonTickets = Orders::leftJoin('customers', function($join){
+        //     $join->on('customers.event_id', '=', 'orders.category_id')
+        //          ->where('customers.order_id', '=', 11111);
+        // })       ->leftJoin('price_sub_categories', 'price_sub_categories.id', '=', 'orders.category_id')
+        // ->leftJoin('zim_events', 'price_sub_categories.event_id', '=', 'zim_events.id')
+        // ->leftJoin('event_types', 'zim_events.event_type_id', '=', 'event_types.id')
+        // ->leftJoin('event_locations', 'event_locations.event_id', '=', 'zim_events.id')
+        // ->select('orders.quantity','zim_events.event_name','price_sub_categories.description','orders.category_id AS catID','customers.user_id')
+        // ->where('orders.user_id','=',$id)
+        // ->where('orders.status','=',0)
+        // ->where('event_types.event_type','=','marathon')
+        // ->get();
+        // return $marathonTickets;
     }
 
     /**
