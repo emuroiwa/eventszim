d564895d771243900944d033df74c9a3b7778448
Ernest <emuroiwa@gmail.com>
2020-02-28 15:13:16 +0200

    apo

--- database/s.sql
+++ database/s.sql
@@ -1,64 +1,65 @@
-INSERT INTO `eventszim`.`event_infos` (
-
+INSERT INTO `eventszim`.`price_sub_categories` (
 	`event_id`,
-	`discounts`,
-	`ticket_collection`,
-	`refund_cancel`,
-	`exchange_upgrade`,
-	`duration`,
-	`gps`,
-	`event_details`,
-	`search_query`,
-	`is_slider`,
-	`event_img`,
-	`event_mobile_img`,
-	`event_card_img`,
-	`event_ticket_img`,
-	`youtube`,
-	`instagram`,
-	`twitter`,
-	`email`,
-	`whatsapp`,
-	`website`,
+	`category_id`,
+	`description`,
+	`price_zwl`,
+	`price_usd`,
+	`max_tickets`,
+	`quantity`,
+	`status`,
 	`created_at`,
 	`updated_at`
 )
 VALUES
 	(
-
 		'8',
-		NULL,
-		NULL,
-		NULL,
-		NULL,
-		NULL,
-		NULL,
-		'The 25th April is set as thee frivolous day of high tea, games, fashion show and wonderful prizes to be won as the Jewelry Auction takes off. Hosted at the Meikles Hotel you know the service is exceptional.  So, all you need to bring along is your finesse, elegance and exceptionally good company, for a relaxing afternoon of delicacies. The event begins at 2:00 pm, in the Palm court and parking is included in the ticket.',
-		NULL,
 		'1',
-		'img/slide/high_tea.png',
-		NULL,
-		'img/slide/card-image/high_tea.png',
-		'img/slide/ticket-img/high_tea_ticket.png',
-		NULL,
-		NULL,
-		NULL,
-		NULL,
-		'0774361648',
-		NULL,
+		'VVIP Seats',
+		'1225.0000',
+		'35.0000',
+		'5',
+		'54',
+		'sale',
 		NULL,
 		NULL
 	);
 
-INSERT INTO `eventszim`.`event_locations` (
+INSERT INTO `eventszim`.`price_sub_categories` (
+	`event_id`,
+	`category_id`,
+	`description`,
+	`price_zwl`,
+	`price_usd`,
+	`max_tickets`,
+	`quantity`,
+	`status`,
+	`created_at`,
+	`updated_at`
+)
+VALUES
+	(
+		'8',
+		'1',
+		'VIP Seats',
+		'927.5000',
+		'26.5000',
+		'5',
+		'48',
+		'sale',
+		NULL,
+		NULL
+	);
+
+INSERT INTO `eventszim`.`price_sub_categories` (
 
 	`event_id`,
-	`venue`,
-	`address1`,
-	`town`,
-	`province`,
-	`country`,
-	`image`,
+	`category_id`,
+	`description`,
+	`price_zwl`,
+	`price_usd`,
+	`max_tickets`,
+	`quantity`,
+	`status`,
 	`created_at`,
 	`updated_at`
 )
@@ -66,12 +67,13 @@ VALUES
 	(
 
 		'8',
-		'Meikles Hotel',
-		NULL,
-		'Harare',
-		NULL,
-		NULL,
-		NULL,
+		'1',
+		'General Seats',
+		'700.0000',
+		'20.0000',
+		'5',
+		'120',
+		'sale',
 		NULL,
 		NULL
 	);
