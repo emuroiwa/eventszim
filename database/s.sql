863662f9d9b942cc8d3b291b360a6df6ab799559
Ernest <emuroiwa@gmail.com>
2020-02-28 12:58:21 +0200

    new table

---
+++ database/s.sql
@@ -0,0 +1,22 @@
+INSERT INTO `eventszim`.`zim_events` (
+	`event_type_id`,
+	`catergory_id`,
+	`event_name`,
+	`start_date`,
+	`end_date`,
+	`popular_rank`,
+	`created_at`,
+	`updated_at`
+)
+VALUES
+	(
+
+		'2',
+		'1',
+		'St Canisius Catholic Church High Tea ',
+		'2020-04-25 14:00:00',
+		'2020-04-25 16:00:00',
+		'1.0000',
+		NULL,
+		NULL
+	);
