63746d33d946c40c0113a63616b65a337b1c3bd7
Ernest <emuroiwa@gmail.com>
2020-03-08 21:20:26 +0200

    sales

---
+++ database/sales.sql
@@ -0,0 +1,7 @@
+DROP VIEW IF EXISTS `sales`;
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost`  VIEW `sales` AS SELECT
+	count(*),psc.description
+FROM
+	`payments`
+INNER JOIN orders ON orders.reference = payments.order_ref
+INNER JOIN price_sub_categories psc on psc.id = orders.category_id WHERE payments.`status`=1 ;
