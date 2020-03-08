dac9de7064db54099d1369d8892cc9aeebcefff4
Ernest <emuroiwa@gmail.com>
2020-03-08 21:34:18 +0200

    runners

--- database/sales.sql
+++ database/sales.sql
@@ -1,7 +1,10 @@
-DROP VIEW IF EXISTS `sales`;
-CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost`  VIEW `sales` AS SELECT
-	count(*),psc.description
+DROP VIEW IF EXISTS `runners`;
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost`  VIEW `runners` AS SELECT
+	psc.description,customers.*
 FROM
 	`payments`
 INNER JOIN orders ON orders.reference = payments.order_ref
-INNER JOIN price_sub_categories psc on psc.id = orders.category_id WHERE payments.`status`=1 GROUP BY psc.description;
+INNER JOIN price_sub_categories psc on psc.id = orders.category_id
+INNER JOIN customers on customers.order_id = payments.order_ref
+
+WHERE payments.`status`=1 ;
