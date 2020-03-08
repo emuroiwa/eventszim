32d1e94e1fcc0e1015f73c68e2f061a5f3cb4b04
Ernest <emuroiwa@gmail.com>
2020-03-08 21:22:46 +0200

    SQL UPDATE

--- database/sales.sql
+++ database/sales.sql
@@ -4,4 +4,4 @@ CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost`  VIEW `sales` AS SELECT
 FROM
 	`payments`
 INNER JOIN orders ON orders.reference = payments.order_ref
-INNER JOIN price_sub_categories psc on psc.id = orders.category_id WHERE payments.`status`=1 ;
+INNER JOIN price_sub_categories psc on psc.id = orders.category_id WHERE payments.`status`=1 GROUP BY psc.description;
