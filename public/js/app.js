2a226bd4acde84765569476bd952e93b53d28180
muroiwa <emuroiwa@gmail.com>
2020-03-10 17:04:21 +0200

    deloitte sql and button decri change

--- public/js/app.js
+++ public/js/app.js
@@ -70950,7 +70950,7 @@ var render = function() {
                 [
                   _vm._v("\n            " + _vm._s(_vm.itemsInCart) + " "),
                   _c("i", { staticClass: "fas fa-shopping-cart d-inline " }),
-                  _vm._v(" Checkout \n            ")
+                  _vm._v(" Proceed to payment \n            ")
                 ]
               )
             ]),
@@ -93952,8 +93952,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\ernestm\Documents\eventsZim\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Users\ernestm\Documents\eventsZim\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\Users\User\Documents\laravel-projects\ticketbook\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Users\User\Documents\laravel-projects\ticketbook\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
